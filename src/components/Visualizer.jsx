c304918b6d07b744f9579fb19914ecb4926c7f55
Rus Milovanovic <cmilovanovic@wisc.edu>
2025-07-14 13:17:57 -0500

    Changed placeholder text.

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Visualizer = () => {
   return (
-    <div style={{ height: '500px', width: '500px'}} /* TODO: remove style when implemented */ 
+    <div style={{ height: '500px', width: '500px', padding: '1rem'}} /* TODO: remove style when implemented */ 
         className="bg-black w-full h-full rounded shadow-md" /* TODO: change w-[full] and h-[full] when implemented */>
       {/* TODO: placeholder for future ROS3D visualization */}
-      <p className="text-white text-center pt-6">Visualizer placeholder</p>
+      <h2 className="text-white text-center pt-6">Visualizer placeholder</h2>
     </div>
   );
 };
